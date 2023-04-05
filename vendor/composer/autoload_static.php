29fb4b0639c56824dc11382c56f055b2c040b3be
Anita-Kozlak <anita.kozlak@gmail.com>
2023-04-06 00:51:40 +0200

    configure database

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -380,8 +380,8 @@ class ComposerStaticInit0fecd51ae3009af0ff024c55e3314274
         ),
         'Illuminate\\Support\\' => 
         array (
-            0 => __DIR__ . '/..' . '/illuminate/macroable',
-            1 => __DIR__ . '/..' . '/illuminate/collections',
+            0 => __DIR__ . '/..' . '/illuminate/collections',
+            1 => __DIR__ . '/..' . '/illuminate/macroable',
             2 => __DIR__ . '/..' . '/illuminate/support',
         ),
         'Illuminate\\Session\\' => 
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'laravel/lumen',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => NULL,
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'e069356b80fb1324c0f7c7ee5d068aa87fbd1817',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -418,9 +418,9 @@
             'dev_requirement' => false,
         ),
         'laravel/lumen' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => NULL,
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'e069356b80fb1324c0f7c7ee5d068aa87fbd1817',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
