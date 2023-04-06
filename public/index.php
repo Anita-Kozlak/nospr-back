7580a08fd612cec3568c4ca81596cbe7103ffe46
Anita-Kozlak <anita.kozlak@gmail.com>
2023-04-06 23:22:44 +0200

    update index.php

--- public/index.php
+++ public/index.php
@@ -1,15 +1,12 @@
 <?php
 
-//Get Heroku ClearDB connection information
-$jawsdb_url = parse_url(getenv("JAWSDB_DATABASE_URL"));
-$jawsdb_server = $jawsdb_url["host"];
-$jawsdb_username = $jawsdb_url["user"];
-$jawsdb_password = $jawsdb_url["pass"];
-$jawsdb_db = substr($jawsdb_url["path"],1);
-$active_group = 'default';
-$query_builder = TRUE;
-// Connect to DB
-$conn = mysqli_connect($jawsdb_server, $jawsdb_username, $jawsdb_password, $jawsdb_db);
+$url = getenv('JAWSDB_URL');
+$dbparts = parse_url($url);
+
+$hostname = $dbparts['host'];
+$username = $dbparts['user'];
+$password = $dbparts['pass'];
+$database = ltrim($dbparts['path'],'/');
 
 /*
 |--------------------------------------------------------------------------
