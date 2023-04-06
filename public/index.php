9bd1fa80cdbc0bcee1a40b72238e302f783b3b82
Anita-Kozlak <anita.kozlak@gmail.com>
2023-04-06 23:47:31 +0200

    update index.php

--- public/index.php
+++ public/index.php
@@ -1,5 +1,12 @@
 <?php
 
+$url = getenv('JAWSDB_URL');
+$dbparts = parse_url($url);
+
+$hostname = $dbparts['host'];
+$username = $dbparts['user'];
+$password = $dbparts['pass'];
+$database = ltrim($dbparts['path'],'/');
 /*
 |--------------------------------------------------------------------------
 | Create The Application
