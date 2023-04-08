565446373855856034fed642e528d896ce8e83ac
Anita-Kozlak <anita.kozlak@gmail.com>
2023-04-08 16:46:42 +0200

    update inde.psp

--- public/index.php
+++ public/index.php
@@ -1,5 +1,4 @@
 <?php
-
 $url = getenv('JAWSDB_URL');
 $dbparts = parse_url($url);
 
@@ -8,39 +7,14 @@ $username = $dbparts['user'];
 $password = $dbparts['pass'];
 $database = ltrim($dbparts['path'],'/');
 
-/*
-|--------------------------------------------------------------------------
-| Create The Application
-|--------------------------------------------------------------------------
-|
-| First we need to get an application instance. This creates an instance
-| of the application / container and bootstraps the application so it
-| is ready to receive HTTP / Console requests from the environment.
-|
-*/
-
-$app = require __DIR__.'/../bootstrap/app.php';
-
-// Set the database configuration
-
 try {
     $pdo = new PDO("mysql:host={$hostname};dbname={$database}", $username, $password);
 } catch(PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
 }
 
-define('ROOT_PATH', dirname(__DIR__));
+echo "Connected successfully to JawsDB MySQL database!";
 
-/*
-|--------------------------------------------------------------------------
-| Run The Application
-|--------------------------------------------------------------------------
-|
-| Once we have the application, we can handle the incoming request
-| through the kernel, and send the associated response back to
-| the client's browser allowing them to enjoy the creative
-| and wonderful application we have prepared for them.
-|
-*/
+// Here, you can add any code that needs to use the database connection.
 
-$app->run();
+?>
