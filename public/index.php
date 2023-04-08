88ed20f847c39a2b7edb0fa5070702acb09e761e
Anita-Kozlak <anita.kozlak@gmail.com>
2023-04-08 17:03:55 +0200

    update index.php

--- public/index.php
+++ public/index.php
@@ -1,5 +1,3 @@
-php
-Copy code
 <?php
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
@@ -13,45 +11,14 @@ $username = $dbparts['user'];
 $password = $dbparts['pass'];
 $database = ltrim($dbparts['path'],'/');
 
-/*
-|--------------------------------------------------------------------------
-| Create The Application
-|--------------------------------------------------------------------------
-|
-| First we need to get an application instance. This creates an instance
-| of the application / container and bootstraps the application so it
-| is ready to receive HTTP / Console requests from the environment.
-|
-*/
-
-$app = require __DIR__.'/../bootstrap/app.php';
-
-// Set the database configuration
-$app->configure('database');
-$app->config['database.connections.mysql.host'] = $hostname;
-$app->config['database.connections.mysql.database'] = $database;
-$app->config['database.connections.mysql.username'] = $username;
-$app->config['database.connections.mysql.password'] = $password;
-
-// Create the database connection
 try {
-    $pdo = new PDO("mysql:host={$hostname};dbname={$database}", $username, $password);
-} catch(PDOException $e) {
+    $conn = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+    }
+catch(PDOException $e)
+    {
     echo "Connection failed: " . $e->getMessage();
-}
-
-define('ROOT_PATH', dirname(__DIR__));
-
-/*
-|--------------------------------------------------------------------------
-| Run The Application
-|--------------------------------------------------------------------------
-|
-| Once we have the application, we can handle the incoming request
-| through the kernel, and send the associated response back to
-| the client's browser allowing them to enjoy the creative
-| and wonderful application we have prepared for them.
-|
-*/
-
-$app->run();
+    }
+?>
