3aaa2cddf26005235ea30cdab38fc3eff4b06999
Anita-Kozlak <anita.kozlak@gmail.com>
2023-04-10 21:32:09 +0200

    test index.php

--- public/index.php
+++ public/index.php
@@ -3,25 +3,14 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
 header("Access-Control-Allow-Headers: Origin, Content-Type, Authorization, access-control-allow-origin");
 
-$url = getenv('JAWSDB_URL');
-$dbparts = parse_url($url);
 
-$hostname = $dbparts['host'];
-$username = $dbparts['user'];
-$password = $dbparts['pass'];
-$database = ltrim($dbparts['path'],'/');
+$app = require __DIR__.'/../bootstrap/app.php';
 
-try {
-    $conn = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully";
-     echo "<br>Hostname: " . $hostname;
-    echo "<br>Database: " . $database;
-    echo "<br>Username: " . $username;
-    echo "<br>Password: " . $password;
-}
-catch(PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
-}
-?>
+define('ROOT_PATH', dirname(__DIR__));
+
+/*
+@@ -32,4 +43,4 @@
+|
+*/
+
+$app->run();
