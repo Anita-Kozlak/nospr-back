b08d81e4c758b099ceddc6e88e45697bbf64104a
Anita-Kozlak <anita.kozlak@gmail.com>
2023-04-10 22:21:47 +0200

    add cleardb

--- public/index.php
+++ public/index.php
@@ -2,15 +2,14 @@
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
 header("Access-Control-Allow-Headers: Origin, Content-Type, Authorization, access-control-allow-origin");
-
-
-$app = require __DIR__.'/../bootstrap/app.php';
-
-define('ROOT_PATH', dirname(__DIR__));
-
-/*
-@@ -32,4 +43,4 @@
-|
-*/
-
-$app->run();
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+?>
